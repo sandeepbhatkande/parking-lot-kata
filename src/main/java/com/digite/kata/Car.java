2e92d5e32826eea62c7de7ca010ee9ffb07fdeb1
Shraddha Jadhav <shraddha.jadhav@digite.com>
2021-06-04 15:27:44 +0530

    TDD1: Failing For getters of CAR Object

---
+++ src/main/java/com/digite/kata/Car.java
@@ -0,0 +1,22 @@
+package com.digite.kata;
+
+public class Car
+{
+    private String m_color;
+    private String m_regNo;
+
+    public Car(String a_color, String a_rego)
+    {
+        this.m_color = a_color;
+        this.m_regNo = a_rego;
+    }
+
+    public String getColor() {
+        return null;
+    }
+
+    public String getRegNo() {
+        return null;
+    }
+
+}
---
+++ src/test/com/digite/kata/TestCar.java
@@ -0,0 +1,22 @@
+package com.digite.kata;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCar
+{
+    @Test
+    public  void testgetCarColor()
+    {
+        Car w_car = new Car("Red", "KA-01-HH-1234");
+        Assertions.assertEquals("Red", w_car.getColor());
+    }
+
+    @Test
+    public  void testgetCarReg()
+    {
+        Car w_car = new Car("Red", "KA-01-HH-1234");
+        Assertions.assertEquals("KA-01-HH-1234", w_car.getColor());
+    }
+}
+
