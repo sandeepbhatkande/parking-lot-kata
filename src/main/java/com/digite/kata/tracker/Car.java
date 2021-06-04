9d714535cbd1705b82ae3dcc808dd0e0ab721abf
Hitesh Narwani <hnarwani@digite.com>
2021-06-04 17:11:07 +0530

    Adding Owner Name in Car Object

--- src/main/java/com/digite/kata/tracker/Car.java
+++ src/main/java/com/digite/kata/tracker/Car.java
@@ -4,9 +4,9 @@ public class Car
 {
     private String m_color;
     private String m_regNo;
+    private String m_ownerName;
 
-
-    public Car(String a_color, String a_rego)
+    public Car(String a_color, String a_rego, String a_ownerName)
     {
         this.m_color = a_color;
         this.m_regNo = a_rego;
@@ -18,6 +18,10 @@ public class Car
 
     public String getRegNo() {
         return m_regNo;
+    }
+    
+    public String getOwnerName() {
+    	return m_ownerName;
     }
 
 }
--- src/test/java/JTestCar.java
+++ src/test/java/JTestCar.java
@@ -10,14 +10,21 @@ public class JTestCar
     @Test
     public  void testgetCarColor()
     {
-        Car w_car = new Car("Red", "KA-01-HH-1234");
+        Car w_car = new Car("Red", "KA-01-HH-1234", "Hitesh Narwani");
         Assertions.assertEquals("Red", w_car.getColor());
     }
 
     @Test
     public  void testgetCarReg()
     {
-        Car w_car = new Car("Red", "KA-01-HH-1234");
+        Car w_car = new Car("Red", "KA-01-HH-1234", "Hitesh Narwani");
         Assertions.assertEquals("KA-01-HH-1234", w_car.getRegNo());
+    }
+    
+    @Test
+    public  void testgetCarOwnerName()
+    {
+        Car w_car = new Car("Red", "KA-01-HH-1234", "Hitesh Narwani");
+        Assertions.assertEquals("Hitesh Narwani", w_car.getOwnerName());
     }
 }
