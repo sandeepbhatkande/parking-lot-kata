1013f2549d062ef106096a878d2ae1bc3b9b9e6b
parth.desai <parth.desai@digite.com>
2021-06-04 08:42:45 +0000

    Car Details

--- src/main/java/com/parkingapp/Car.java
+++ src/main/java/com/parkingapp/Car.java
@@ -5,8 +5,16 @@ public class Car {
 	private String registrationNumber;
 	private String color;
 	
-	public Car(String registrationNumber, String Color) {
+	public Car(String registrationNumber, String color) {
 		this.registrationNumber = registrationNumber;
 		this.color = color; 
+	}
+
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public String getColor() {
+		return color;
 	}
 }
--- src/test/java/com/parkingapp/CarTest.java
+++ src/test/java/com/parkingapp/CarTest.java
@@ -1,14 +1,21 @@
 package com.parkingapp;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class CarTest {
+public class CarTest {
 
 	@Test
 	public void testCreationOfCar() {
 		Car car = new Car("KA-01-HH-1234", "White");
+	}
+	
+	@Test
+	public void testCarDetails() {
+		Car car = new Car("KA-01-HH-1234", "White");
+		assertTrue("KA-01-HH-1234".equals(car.getRegistrationNumber()));
+		assertTrue("White".equals(car.getColor()));
 	}
 
 }
