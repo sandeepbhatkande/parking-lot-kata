6c54625f760454415346b9cb11748409d0f436c3
Vishal VD <vishal.dandanaik@digite.com>
2021-06-04 17:42:22 +0530

    Car

---
+++ test/TestCar.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestCar
+{
+
+    @Test
+    public void newcar() {
+        Car c = new Car("car1", "white");
+    }
+
+    @Test
+    public void cardetails() {
+        Car c = new Car("car1","black"  );
+        assertTrue("car1".equals(c.getRegistrationNo()));
+        assertTrue("white".equals(c.getColor()));
+    }
+
+}
