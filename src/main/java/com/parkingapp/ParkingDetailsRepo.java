dba704a78e7cd7eef1a78c282f7c9355b493871d
parth.desai <parth.desai@digite.com>
2021-06-04 10:14:58 +0000

    ParkingDetailsRepo

---
+++ src/main/java/com/parkingapp/ParkingDetailsRepo.java
@@ -0,0 +1,27 @@
+package com.parkingapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ParkingDetailsRepo {
+	
+	private ArrayList<ParkingDetails> repo;
+	private HashMap<Integer, ParkingDetails> parkedCars;
+	
+	public ParkingDetailsRepo() {
+		repo = new ArrayList<ParkingDetails>();
+		parkedCars = new HashMap<Integer, ParkingDetails>();
+	}
+
+	public void addDetails(ParkingDetails parkingDetails) {
+		repo.add(parkingDetails);
+		if(parkingDetails.isParked()) {
+			parkedCars.put(parkingDetails.getSlotId(), parkingDetails);
+		}
+	}
+	
+	public ParkingDetails getDetailsBySlot(int slotId) {
+		return parkedCars.get(slotId);
+	}
+	
+}
---
+++ src/test/java/com/parkingapp/ParkingDetailsRepoTest.java
@@ -0,0 +1,37 @@
+package com.parkingapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParkingDetailsRepoTest {
+
+	@Test
+	public void testParkingDetailsRepo() {
+		Car car = new Car("KA-01-HH-1234", "White");
+		int slotId = 10;
+		boolean parked = true;
+		
+		ParkingDetails parkingDetails = new ParkingDetails(car, slotId, parked);
+		
+		ParkingDetailsRepo parkingDetailsRepo = new ParkingDetailsRepo();
+		parkingDetailsRepo.addDetails(parkingDetails);
+	}
+
+	@Test
+	public void testGetDetailsBySlot() {
+		Car car = new Car("KA-01-HH-1234", "White");
+		int slotId = 10;
+		boolean parked = true;
+		
+		ParkingDetails parkingDetails = new ParkingDetails(car, slotId, parked);
+		
+		ParkingDetailsRepo parkingDetailsRepo = new ParkingDetailsRepo();
+		parkingDetailsRepo.addDetails(parkingDetails);
+		
+		ParkingDetails responseDetails = parkingDetailsRepo.getDetailsBySlot(slotId);
+		assertTrue(responseDetails.getCar().equals(car));
+		assertTrue(responseDetails.getSlotId() == slotId);
+		assertTrue(responseDetails.isParked() == true);
+	}
+}
