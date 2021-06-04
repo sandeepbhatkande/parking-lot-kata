a7bb3cbd2fdbd2da864837f1d5e30ca6a1045f26
Hitesh Narwani <hnarwani@digite.com>
2021-06-04 16:15:13 +0530

    Code changes for making TDD pass for parkingslots creation

--- src/main/java/com/digite/kata/tracker/ParkingLot.java
+++ src/main/java/com/digite/kata/tracker/ParkingLot.java
@@ -12,6 +12,12 @@ public class ParkingLot
     	if (parkingSlot < 0)
     		parkingSlot = 0;
         this.parkingSlot = parkingSlot;
+        
+        m_parkingSlotList = new ArrayList<ParkingSlot>();
+        for (int i = 0; i < parkingSlot; i++)
+        {
+        	m_parkingSlotList.add(new ParkingSlot(i,true));
+        }
     }
     
     public int getParkingSlot() {
